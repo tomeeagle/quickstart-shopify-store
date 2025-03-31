55605a86dc00eeb8f04c0c24341cbac36a7c1ae4
tomeeagle <tomeagleweb@gmail.com>
2025-03-31 11:35:17 +0100

    Create web component that reads the RVP from local storage

---
+++ assets/recently-viewed.js
@@ -0,0 +1,34 @@
+// Web component that gets recently viewed products from the Shopify API and displays them in a grid
+class RecentlyViewedProducts extends HTMLElement {
+  constructor() {
+    super();
+    this.recentlyViewedProducts = [];
+    this.sectionId = this.closest('.shopify-section').id.replace('shopify-section-', '');
+    this.recentlyViewedProducts = JSON.parse(localStorage.getItem(`recently_viewed_products`)) || [];
+    this.render();
+  }
+
+  render() {
+    console.log(this.recentlyViewedProducts);
+    if (this.recentlyViewedProducts.length === 0) return;
+
+    const productGrid = document.createElement('div');
+    productGrid.classList.add('recently-viewed-products-grid');
+
+    this.recentlyViewedProducts.forEach((product) => {
+      const productCard = document.createElement('div');
+      productCard.classList.add('product-card');
+      productCard.innerHTML = `
+        <a href="${product.productUrl}">
+          <img src="${product.productImg}" alt="${product.productTitle}">
+          <h2>${product.productTitle}</h2>
+        </a>
+      `;
+      productGrid.appendChild(productCard);
+    });
+
+    console.log(productGrid);
+    this.appendChild(productGrid);
+  }
+}
+customElements.define('recently-viewed-products', RecentlyViewedProducts);
